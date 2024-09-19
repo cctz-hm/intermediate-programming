a2215a5d152de9912d0e27c6525bbe17b48b3fc0
cctz-hm <ciana_tzuO@horacemann.org>
2024-09-19 13:50:57 -0400

    shellscript.sh

--- shellscript.sh
+++ shellscript.sh
@@ -7,4 +7,4 @@ echo "Enter your favorite flavor of ice cream"
 read ice_cream
 echo $ice_cream
 
-say -v Kyoko  "Welcome $name"
+say -v Kyoko  "Welcome $name. I don't like your ice cream flavor $ice_cream"
