35bf82304412f762fb401e7e8715e640d0f13424
cctz-hm <ciana_tzuO@horacemann.org>
2024-10-31 12:59:51 -0400

    MovieDownload

---
+++ MovieDownload/src/App.java
@@ -0,0 +1,31 @@
+
+class App {
+    public static void main(String[] args) throws Exception {
+        // 1. Create a new MoviePosterWriter
+        MoviePosterWriter mpw = new MoviePosterWriter();
+        // 2. Create a new Movie, pass it in a string of a movie title
+        Movie avengers = new Movie("avengers");
+        // 3. use the movie poster writer to set a movie and then write it
+        mpw.setMovieString(avengers.getMovieNameForURL());
+        mpw.write(avengers.getMovieFilename());
+        
+        // Choose your own movies below and put them into this array! make a loop to repeat steps 2 and 3.
+        String[] movies = { "avengers", "Deadpool", "Spider man into the spider verse", "despicable me", "inside out" };
+        for (int i = 0; i < movies.length; i++) {
+            Movie m = new Movie(movies[i]);
+            mpw.setMovieString(m.getMovieNameForURL());
+            mpw.write(m.getMovieFilename());
+
+
+        }
+       
+        // YOUR CODE HERE: in a loop, do steps 2 and 3 for every movie
+        
+        // the code currently doesn't work if you have a space in the movie name. see below
+        // you need to fix getMovieNameForURL() and getMovieFilename() in the Movie.java file.
+        // Movie airbud = new Movie("air bud");
+        // mpw.setMovieString("air bud");
+        // mpw.write("air bud");
+        
+    }
+}
---
+++ MovieDownload/src/Movie.java
@@ -0,0 +1,42 @@
+class Movie {
+
+    // fields 
+    String movieName;
+    String movieNameForUrl;
+    String movieScreenshotFilename;
+    
+    Movie(String name) {
+        this.movieName = name;
+        this.movieNameForUrl = getMovieNameForURL();
+        this.movieScreenshotFilename = getMovieFilename();
+    }
+
+
+
+    /* If there are spaces in the movie, the URL parsing fails.
+     * Use String methods to replace any spaces in the movieName to 
+     * "%20" or plus signs "+" to make it URL-safe, and return that string.
+     * see https://www.w3schools.com/tags/ref_urlencode.ASP
+     */
+    String getMovieNameForURL() {
+        movieName = movieName.replace(" ", "%20");
+        
+        return movieName;
+    } 
+
+    /* If the movie doesn't have an extension, you cannot view it in VSCode correctly. 
+    This method should add a ".jpg" or ".png" extension to the movie name to make it clear this is a picture.
+    Also, remove spaces since spaces in filenames are ANNOYING. maybe replace with underscores?
+    */
+    String getMovieFilename() {
+        String fileName = movieName.replace(" ", "_");
+        if (!(fileName.endsWith(".jpg") || fileName.endsWith(".png"))) {
+            fileName += ".jpg";
+        }
+        
+        return fileName;
+    } 
+
+
+
+}
