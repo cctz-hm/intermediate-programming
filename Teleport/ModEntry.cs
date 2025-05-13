f2881c92457edc7a1e9bf5c876640d258084b141
cctz-hm <ciana_tzuO@horacemann.org>
2025-05-13 14:15:10 -0400

    Teleport mod

---
+++ Teleport/ModEntry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace Teleport
+{
+    /// <summary>The mod entry point.</summary>
+    internal sealed class MonEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+
+            if (e.Button == SButton.H)
+            Game1.warpFarmer("FarmHouse", 64, 15, false);
+        }
+    }
+}
